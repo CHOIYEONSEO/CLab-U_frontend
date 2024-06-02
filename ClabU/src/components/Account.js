382b81d5fca5248643ad05850e4958f5390dc239
monocerotis20 <monocerotis@g.skku.edu>
2024-06-03 01:39:41 +0900

    fixed logout screen

--- ClabU/src/components/Account.js
+++ ClabU/src/components/Account.js
@@ -1,17 +1,29 @@
 import PropTypes from "prop-types";
 import styles from "./Account.module.css";
+import { useNavigate } from "react-router-dom";
+import { useCallback } from "react";
 
-const Account = ({ className = "" }) => {
+const Account = ({ className = ""}) => {
+  const navigate = useNavigate();
+  const onCancelClick = useCallback(() => {
+    navigate(0);
+  }, [navigate]);
+
+  const onLogOutClick = useCallback(() => {
+    navigate(0);
+  }, [navigate]);
+
   return (
     <div className={[styles.account, className].join(" ")}>
-      <div className={styles.npc}>안녕하세요, NPC 님.</div>
+      <div className={styles.npc}>안녕하세요, NPC 님!</div>
       <img
-        className={styles.freeIconX38189271}
+        className={styles.CloseIcon}
         alt=""
         src="/freeiconx3818927-1@2x.png"
+        onClick={onCancelClick}
       />
       <img className={styles.account2Icon} alt="" src="/account-2@2x.png" />
-      <div className={styles.div}>로그아웃하기</div>
+      <div className={styles.div} onClick={onLogOutClick}>로그아웃하기</div>
     </div>
   );
 };
