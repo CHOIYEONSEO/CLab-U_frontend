75e87750968424b80d6c7ff5bdf867a3d9237562
Kim Jeong-won <threedalpeng@gmail.com>
2024-06-03 21:11:49 +0900

    refact: rebase to vite

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api/chat": {
+        target: "http://3.38.183.31:8000",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://3.38.183.31:3000",
+        changeOrigin: true,
+      },
+      "/image": {
+        target: "http://3.38.183.31:8000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
