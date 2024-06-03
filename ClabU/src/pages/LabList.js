8534e39bfc6ac455bf7c1e6b25acf18d45b08c31
변지환 <99013865+Quswlghks@users.noreply.github.com>
2024-06-03 20:00:57 +0900

    LabList component 개선

--- ClabU/src/pages/LabList.js
+++ ClabU/src/pages/LabList.js
@@ -49,6 +49,66 @@ const LabList = () => {
 
   return (
     <div className={styles.labList}>
+      <div className={styles.labItem}>
+        <div className={styles.labName}>인공지능을활용한보안</div>
+        <img
+          className={styles.labImage}
+          alt=""
+          src="/@2x.png"
+          onClick={onImageClick}
+        />
+      </div>
+      <div className={styles.labItem}>
+        <div className={styles.labName}>데이터기반융합보안</div>
+        <img
+          className={styles.labImage}
+          alt=""
+          src="/-6@2x.png"
+          onClick={onImageClick}
+        />
+      </div>
+      <div className={styles.labItem}>
+        <div className={styles.labName}>컴퓨팅플랫폼</div>
+        <img
+          className={styles.labImage}
+          alt=""
+          src="/-5@2x.png"
+          onClick={onImageClick}
+        />
+      </div>
+      <div className={styles.labItem}>
+        <div className={styles.labName}>시스템보안</div>
+        <img
+          className={styles.labImage}
+          alt=""
+          src="/-4@2x.png"
+          onClick={onImageClick}
+        />
+      </div>
+      <div className={styles.labItem}>
+        <div className={styles.labName}>소프트웨어공학</div>
+        <img
+          className={styles.labImage}
+          alt=""
+          src="/-3@2x.png"
+          onClick={onImageClick}
+        />
+      </div>
+      <div className={styles.labItem}>
+        <div className={styles.labName}>자연어처리연구실</div>
+        <img
+          className={styles.labImage}
+          alt=""
+          src="/-2@2x.png"
+          onClick={onImageClick}
+        />
+      </div>
+      <div className={styles.tab}>
+        <div className={styles.club} onClick={onClubTextClick}>
+          club
+        </div>
+        <b className={styles.lab}>lab</b>
+      </div>
       <Navigation1
         logIn="/login@2x.png"
         account1="/account-1@2x.png"
@@ -61,79 +121,20 @@ const LabList = () => {
         onClabUTextClick={onClabUTextClick}
         onLogoIconClick={onLogoIconClick}
       />
-      <div className={styles.header}>
-        <div className={styles.tab}>
-          <div className={styles.club} onClick={onClubTextClick}>
-            club
-          </div>
-          <b className={styles.lab}>lab</b>
-        </div>
-        <div className={styles.nameSearch}>
-          <img
-            className={styles.search154734Icon}
-            alt=""
-            src="/search-154734@2x.png"
-          />
-          <input
-            className={styles.typeHere}
-            placeholder="연구실명으로 검색"
-            type="text"
-            value={labQuery}
-            onChange={(e) => setlabQuery(e.target.value)}
-            onKeyDown={(e) => activeEnter(e)}
-          />
-        </div>
-      </div>
-      <div className={styles.labGrid}>
-        <div className={styles.labItem}>
-          <div className={styles.labName}>인공지능을활용한보안</div>
-          <img
-            className={styles.labImage}
-            alt=""
-            src="/@2x.png"
-            onClick={onImageClick}
-          />
-        </div>
-        <div className={styles.labItem}>
-          <div className={styles.labName}>데이터기반융합보안</div>
-          <img
-            className={styles.labImage}
-            alt=""
-            src="/-6@2x.png"
-          />
-        </div>
-        <div className={styles.labItem}>
-          <div className={styles.labName}>컴퓨팅플랫폼</div>
-          <img
-            className={styles.labImage}
-            alt=""
-            src="/-5@2x.png"
-          />
-        </div>
-        <div className={styles.labItem}>
-          <div className={styles.labName}>시스템보안</div>
-          <img
-            className={styles.labImage}
-            alt=""
-            src="/-4@2x.png"
-          />
-        </div>
-        <div className={styles.labItem}>
-          <div className={styles.labName}>소프트웨어공학</div>
-          <img
-            className={styles.labImage}
-            alt=""
-            src="/-3@2x.png"
-          />
-        </div>
-        <div className={styles.labItem}>
-          <div className={styles.labName}>자연어처리연구실</div>
-          <img
-            className={styles.labImage}
-            alt=""
-            src="/-2@2x.png"
-          />
-        </div>
+      <div className={styles.nameSearch}>
+        <img
+          className={styles.search154734Icon}
+          alt=""
+          src="/search-154734@2x.png"
+        />
+        <input
+          className={styles.typeHere}
+          placeholder="연구실명으로 검색"
+          type="text"
+          value={labQuery}
+          onChange={(e) => setlabQuery(e.target.value)}
+          onKeyDown={(e) => activeEnter(e)}
+        />
       </div>
     </div>
   );
