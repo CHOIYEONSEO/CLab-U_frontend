079da15b8294a756c04a20ae95a6144c41a08f29
변지환 <99013865+Quswlghks@users.noreply.github.com>
2024-06-04 17:28:08 +0900

    refac: login component

--- src/features/auth/pages/ApplicationLogin.jsx
+++ src/features/auth/pages/ApplicationLogin.jsx
@@ -20,49 +20,49 @@ const ApplicationLogin = () => {
 
   return (
     <div className={styles.applicationLogin}>
-      <div className={styles.boundary}>
-        <b className={styles.b}>
-          아이디와 비밀번호 찾기는 관리자에게 문의하세요
-        </b>
-      </div>
-      <div className={styles.login} to="/">
-        <b className={styles.b1}>로그인</b>
-      </div>
-      <div className={styles.passwd}>
-        <input
-          className={styles.passwdInput}
-          onFocus={() => {
-            setIsPasswdInputClicked(true);
-          }}
-          // 클릭되어 있지 않을 때 작동(input 이외의 영역이 클릭되었을 때)
-          onBlur={() => {
-            setIsPasswdInputClicked(false);
-          }}
-          placeholder={isPasswdInputClicked === true ? "" : "비밀번호"}
-          type="password"
-          name="passwd"
-          value={logInForm.password}
-          onChange={handleChange}
-        />
-        <img className={styles.passwdIcon} alt="" src="/passwd-icon@2x.png" />
-      </div>
-      <div className={styles.id}>
-        <input
-          className={styles.idInput}
-          onFocus={() => {
-            setIsIdInputClicked(true);
-          }}
-          // 클릭되어 있지 않을 때 작동(input 이외의 영역이 클릭되었을 때)
-          onBlur={() => {
-            setIsIdInputClicked(false);
-          }}
-          placeholder={isIdInputClicked === true ? "" : "아이디"}
-          type="text"
-          name="id"
-          value={logInForm.id}
-          onChange={handleChange}
-        />
-        <img className={styles.idIcon} alt="" src="/id-icon@2x.png" />
+      <div className={styles.content}>
+        <div className={styles.loginForm}>
+          <div className={styles.id}>
+            <input
+              className={styles.idInput}
+              onFocus={() => {
+                setIsIdInputClicked(true);
+              }}
+              onBlur={() => {
+                setIsIdInputClicked(false);
+              }}
+              placeholder={isIdInputClicked === true ? "" : "아이디"}
+              type="text"
+              name="id"
+              value={logInForm.id}
+              onChange={handleChange}
+            />
+            <img className={styles.idIcon} alt="" src="/id-icon@2x.png" />
+          </div>
+          <div className={styles.passwd}>
+            <input
+              className={styles.passwdInput}
+              onFocus={() => {
+                setIsPasswdInputClicked(true);
+              }}
+              onBlur={() => {
+                setIsPasswdInputClicked(false);
+              }}
+              placeholder={isPasswdInputClicked === true ? "" : "비밀번호"}
+              type="password"
+              name="password"
+              value={logInForm.password}
+              onChange={handleChange}
+            />
+            <img className={styles.passwdIcon} alt="" src="/passwd-icon@2x.png" />
+          </div>
+          <div className={styles.login}>
+            <b className={styles.loginText}>로그인</b>
+          </div>
+            <b className={styles.boundaryText}>
+              아이디와 비밀번호 찾기는 관리자에게 문의하세요
+            </b>
+        </div>
       </div>
     </div>
   );
